a04b8a7082d0a9c31ba0fa0eb87a54755d459a0a
Maxim Klenov <maxklenow@gmail.com>
2025-04-04 00:28:56 +0300

    Delete utils.js

--- utils.js
+++
@@ -1,9 +0,0 @@
-async function sendWelcomeMessage(ctx) {
-  // Продолжить выполнение кода
-}
-
-async function sendRestartMessage(ctx) {
-  // Продолжить выполнение кода
-}
-
-module.exports = { sendWelcomeMessage, sendRestartMessage };
